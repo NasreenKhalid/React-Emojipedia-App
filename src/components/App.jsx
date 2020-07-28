d7f26ead08e5b24cbd061da0e375eb0604d965aa
Nasreen Khalid <nasreen.1@live.com>
2020-07-28 22:51:14 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Term from "./Term";
+import emojipedia from "../emojipedia";
+
+function createTerm(term) {
+  return (
+    <Term
+      key={term.id}
+      emoji={term.emoji}
+      name={term.name}
+      meaning={term.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createTerm)}
+        {/* <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div> */}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "😂",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🍎",
+    name: "Red Apple",
+    meaning:
+      "Of symbolic importance: stands for love, temptation, the fall of mankind and the expulsion from paradise. In Latin, apple means “bad, fatal, evil“. Snow White is poisoned by the queen with a red apple. Is also used in its direct meaning or for a healthy lifestyle.	",
+  },
+  {
+    id: 5,
+    emoji: "💜",
+    name: "Purple Heart",
+    meaning:
+      "Expression of understanding and compassionate love. Represents affection and care. For Mother's Day or in messages between parents and children. Used by fashion-conscious people or celebrities as an expression of glamor or wealth. In the US, military medal for wounded soldiers.",
+  },
+  {
+    id: 6,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A sweet dog face with a panting tongue and floppy ears. Stands for unconditional loyalty and attention. Can refer to your own pet (“I love my dog.“) or to people (“You sly dog!“).",
+  },
+];
+
+export default emojipedia;
